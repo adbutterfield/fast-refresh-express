7dc7f7da333a678dc93f6f25f3d78d3c53b9f4f9
Adam Butterfield <adam.a.butterfield@axa.co.jp>
2021-04-14 09:20:52 +0900

    Only add the compiler hook in dev

--- server/devMiddleware.ts
+++ server/devMiddleware.ts
@@ -11,13 +11,15 @@ const compiler = webpack(config);
 
 const dirName = path.resolve(__dirname, "../react/");
 
-compiler.hooks.afterEmit.tap("cleanup-the-require-cache", () => {
-  // After webpack rebuild, clear the files from the require cache,
-  // so that next server side render wil be in sync
-  Object.keys(require.cache)
-    .filter((key) => key.includes(dirName))
-    .forEach((key) => delete require.cache[key]);
-});
+if (isDev) {
+  compiler.hooks.afterEmit.tap("cleanup-the-require-cache", () => {
+    // After webpack rebuild, clear the files from the require cache,
+    // so that next server side render wil be in sync
+    Object.keys(require.cache)
+      .filter((key) => key.includes(dirName))
+      .forEach((key) => delete require.cache[key]);
+  });
+}
 
 const addDevMiddleware = (app: Express): void => {
   if (isDev) {
