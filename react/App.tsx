63b56c572122b03c288af5cf8bdcdaa59907048b
Adam Butterfield <adam.butterfield@ymail.com>
2022-06-02 23:51:45 +0900

    Move Suspense and ErrorBoundary

--- react/App.tsx
+++ react/App.tsx
@@ -1,22 +1,25 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Top = lazy(() => import("./Top"));
 const Page = lazy(() => import("./Page"));
 
 const App: React.FC = () => (
-  <>
-    <Link to="/">Link to Top</Link>
-    <br />
-    <br />
-    <Link to="/page">Link to Page</Link>
-    <br />
-    <br />
-    <Routes>
-      <Route path="/" element={<Top />} />
-      <Route path="/page" element={<Page />} />
-    </Routes>
-  </>
+  <Suspense fallback={<div>Suspending...</div>}>
+    <ErrorBoundary>
+      <Link to="/">Link to Top</Link>
+      <br />
+      <br />
+      <Link to="/page">Link to Page</Link>
+      <br />
+      <br />
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/page" element={<Page />} />
+      </Routes>
+    </ErrorBoundary>
+  </Suspense>
 );
 
 export default App;
--- react/index.tsx
+++ react/index.tsx
@@ -1,20 +1,15 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { hydrateRoot } from "react-dom/client";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
 
 const root = document.getElementById("react-app");
 
 if (root) {
   hydrateRoot(
     root,
-    <ErrorBoundary>
-      <Suspense fallback={<div>Suspending...</div>}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Suspense>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 }
