ba768517aff55826287da54a6e258f5da2101770
Adam Butterfield <adam.butterfield@ymail.com>
2023-03-05 01:03:24 +0900

    Add styled component with props

--- react/App.tsx
+++ react/App.tsx
@@ -1,39 +1,48 @@
 import React, { lazy, Suspense } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
+import { LinkCmp } from "./components";
 
 const Top = lazy(() => import("./Top"));
 const Page = lazy(() => import("./Page"));
 
-const App: React.FC = () => (
-  <Suspense fallback={<div>Suspending...</div>}>
-    <ErrorBoundary>
-      <Link to="/">Link to Top</Link>
-      <br />
-      <br />
-      <Link to="/page">Link to Page</Link>
-      <br />
-      <br />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<div>Suspending Top...</div>}>
-              <Top />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/page"
-          element={
-            <Suspense fallback={<div>Suspending Page...</div>}>
-              <Page />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </ErrorBoundary>
-  </Suspense>
-);
+const App: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <Suspense fallback={<div>Suspending...</div>}>
+      <ErrorBoundary>
+        <LinkCmp isActive={location.pathname === "/"} to="/">
+          Link to Top
+        </LinkCmp>
+        <br />
+        <br />
+        <LinkCmp isActive={location.pathname === "/page"} to="/page">
+          Link to Page
+        </LinkCmp>
+        <br />
+        <br />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<div>Suspending Top...</div>}>
+                <Top />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/page"
+            element={
+              <Suspense fallback={<div>Suspending Page...</div>}>
+                <Page />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
 
 export default App;
--- react/components.tsx
+++ react/components.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Cmp = styled.div`
   color: red;
+`;
+
+export const LinkCmp = styled(Link).withConfig({
+  shouldForwardProp: (prop, defaultValidatorFn) =>
+    !["isActive"].includes(prop) && defaultValidatorFn(prop),
+})<{ isActive: boolean }>`
+  ${({ isActive }) => isActive && `font-weight: bold;`}
 `;
 
 export default Cmp;
