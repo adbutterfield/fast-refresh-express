5564d33abfd1296b1edc0419d01ad824c30205e7
Adam Butterfield <adam.butterfield@ymail.com>
2023-03-16 23:16:08 +0900

    Update snapshots

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -14,20 +14,20 @@ describe("routes", () => {
           <meta charset="UTF-8">
           <title>Fast Refresh Express</title>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <style data-styled="true" data-styled-version="5.3.8">.bvGZpp{font-weight:bold;}/*!sc*/
-      data-styled.g1[id="Link__LinkCmp-sc-c21fd39b-0"]{content:"bvGZpp,dqqGHD,"}/*!sc*/
-      @media (max-width:767px){.bqDvgf{margin:1rem 0;font-size:1.5rem;}}/*!sc*/
-      @media (min-width:768px){.bqDvgf{margin:2rem 0;font-size:2rem;}}/*!sc*/
-      data-styled.g2[id="PageTitle-sc-6479891d-0"]{content:"bqDvgf,"}/*!sc*/
-      .fxbHAk{color:rgb(0,0,143);font-size:1.75rem;}/*!sc*/
-      @media (min-width:768px){.fxbHAk{text-align:center;font-size:2.25rem;}}/*!sc*/
-      data-styled.g3[id="PageTitle__StyledPageTitle-sc-6479891d-1"]{content:"fxbHAk,"}/*!sc*/
-      .bLpLVf{color:red;}/*!sc*/
-      data-styled.g4[id="Cmp-sc-4e3bc353-0"]{content:"bLpLVf,"}/*!sc*/
+          <style data-styled="true" data-styled-version="5.3.9">.gCRJcT{font-weight:bold;}/*!sc*/
+      data-styled.g1[id="Link__LinkCmp-sc-19ap5pz-0"]{content:"gCRJcT,"}/*!sc*/
+      @media (max-width:767px){.hYSwJc{margin:1rem 0;font-size:1.5rem;}}/*!sc*/
+      @media (min-width:768px){.hYSwJc{margin:2rem 0;font-size:2rem;}}/*!sc*/
+      data-styled.g2[id="PageTitle-sc-10824m2-0"]{content:"hYSwJc,"}/*!sc*/
+      .kwqGsj{color:rgb(0,0,143);font-size:1.75rem;}/*!sc*/
+      @media (min-width:768px){.kwqGsj{text-align:center;font-size:2.25rem;}}/*!sc*/
+      data-styled.g3[id="PageTitle__StyledPageTitle-sc-10824m2-1"]{content:"kwqGsj,"}/*!sc*/
+      .ioTYJi{color:red;}/*!sc*/
+      data-styled.g4[id="Cmp-sc-ysn75t-0"]{content:"ioTYJi,"}/*!sc*/
       </style>
         </head>
         <body>
-          <div id="react-app"><!--$--><h1 class="PageTitle-sc-6479891d-0 PageTitle__StyledPageTitle-sc-6479891d-1 bqDvgf fxbHAk">Fast Refresh Express</h1><a class="Link__LinkCmp-sc-c21fd39b-0 bvGZpp" href="/">Link to Top</a><br/><br/><a class="Link__LinkCmp-sc-c21fd39b-0 dqqGHD" href="/page">Link to Page</a><br/><br/><!--$--><div data-testid="content" class="Cmp-sc-4e3bc353-0 bLpLVf">TOP</div><!--/$--><!--/$--><script src="/main.js" async=""></script><script src="/runtime~main.js" async=""></script></div>
+          <div id="react-app"><!--$--><h1 class="PageTitle-sc-10824m2-0 PageTitle__StyledPageTitle-sc-10824m2-1 hYSwJc kwqGsj">Fast Refresh Express</h1><a class="Link__LinkCmp-sc-19ap5pz-0 gCRJcT" href="/">Link to Top</a><br/><br/><a class="Link__LinkCmp-sc-19ap5pz-0" href="/page">Link to Page</a><br/><br/><!--$--><div data-testid="content" class="Cmp-sc-ysn75t-0 ioTYJi">TOP</div><!--/$--><!--/$--><script src="/main.js" async=""></script><script src="/runtime~main.js" async=""></script></div>
         </body>
       </html>"
     `);
@@ -53,20 +53,20 @@ describe("routes", () => {
           <meta charset="UTF-8">
           <title>Fast Refresh Express</title>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <style data-styled="true" data-styled-version="5.3.8">.bvGZpp{font-weight:bold;}/*!sc*/
-      data-styled.g1[id="Link__LinkCmp-sc-c21fd39b-0"]{content:"dqqGHD,bvGZpp,"}/*!sc*/
-      @media (max-width:767px){.bqDvgf{margin:1rem 0;font-size:1.5rem;}}/*!sc*/
-      @media (min-width:768px){.bqDvgf{margin:2rem 0;font-size:2rem;}}/*!sc*/
-      data-styled.g2[id="PageTitle-sc-6479891d-0"]{content:"bqDvgf,"}/*!sc*/
-      .fxbHAk{color:rgb(0,0,143);font-size:1.75rem;}/*!sc*/
-      @media (min-width:768px){.fxbHAk{text-align:center;font-size:2.25rem;}}/*!sc*/
-      data-styled.g3[id="PageTitle__StyledPageTitle-sc-6479891d-1"]{content:"fxbHAk,"}/*!sc*/
-      .bLpLVf{color:red;}/*!sc*/
-      data-styled.g4[id="Cmp-sc-4e3bc353-0"]{content:"bLpLVf,"}/*!sc*/
+          <style data-styled="true" data-styled-version="5.3.9">.gCRJcT{font-weight:bold;}/*!sc*/
+      data-styled.g1[id="Link__LinkCmp-sc-19ap5pz-0"]{content:"gCRJcT,"}/*!sc*/
+      @media (max-width:767px){.hYSwJc{margin:1rem 0;font-size:1.5rem;}}/*!sc*/
+      @media (min-width:768px){.hYSwJc{margin:2rem 0;font-size:2rem;}}/*!sc*/
+      data-styled.g2[id="PageTitle-sc-10824m2-0"]{content:"hYSwJc,"}/*!sc*/
+      .kwqGsj{color:rgb(0,0,143);font-size:1.75rem;}/*!sc*/
+      @media (min-width:768px){.kwqGsj{text-align:center;font-size:2.25rem;}}/*!sc*/
+      data-styled.g3[id="PageTitle__StyledPageTitle-sc-10824m2-1"]{content:"kwqGsj,"}/*!sc*/
+      .ioTYJi{color:red;}/*!sc*/
+      data-styled.g4[id="Cmp-sc-ysn75t-0"]{content:"ioTYJi,"}/*!sc*/
       </style>
         </head>
         <body>
-          <div id="react-app"><!--$--><h1 class="PageTitle-sc-6479891d-0 PageTitle__StyledPageTitle-sc-6479891d-1 bqDvgf fxbHAk">Fast Refresh Express</h1><a class="Link__LinkCmp-sc-c21fd39b-0 dqqGHD" href="/">Link to Top</a><br/><br/><a class="Link__LinkCmp-sc-c21fd39b-0 bvGZpp" href="/page">Link to Page</a><br/><br/><!--$--><div data-testid="content" class="Cmp-sc-4e3bc353-0 bLpLVf">PAGE</div><!--/$--><!--/$--><script src="/main.js" async=""></script><script src="/runtime~main.js" async=""></script></div>
+          <div id="react-app"><!--$--><h1 class="PageTitle-sc-10824m2-0 PageTitle__StyledPageTitle-sc-10824m2-1 hYSwJc kwqGsj">Fast Refresh Express</h1><a class="Link__LinkCmp-sc-19ap5pz-0" href="/">Link to Top</a><br/><br/><a class="Link__LinkCmp-sc-19ap5pz-0 gCRJcT" href="/page">Link to Page</a><br/><br/><!--$--><div data-testid="content" class="Cmp-sc-ysn75t-0 ioTYJi">PAGE</div><!--/$--><!--/$--><script src="/main.js" async=""></script><script src="/runtime~main.js" async=""></script></div>
         </body>
       </html>"
     `);
